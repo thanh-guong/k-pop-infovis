feadd39eb97b83a8d910069b53bc7d0680596400
Marte Valerio Falcone <marte.vfalcone@gmail.ocm>
2020-07-10 13:08:25 +0200

    Finally got a graph

--- js/k-pop.js
+++ js/k-pop.js
@@ -13,9 +13,8 @@ let k_elastic;
 let spring_length;
 let gravity;
 
-let svg;
-let width = 900;
-let height = 600;
+let width = "1600";
+let height = "800";
 
 /**************************
  **************************
@@ -224,112 +223,184 @@ function initializeGlobalVariablesFromDOM()
     gravity = document.getElementById("gravity").value;
 }
 
-/***
- * NO SENSE, AND NON USED
- */
-function incidenceMatrix()
-{
-    let incidence_matrix = {}
+/***************************************************
+ ******************* MAIN **************************
+ ***************************************************/
 
-    for (let i = 0; i < edges.length; i++)
-    {
-        edges[i].source = nodes[edges[i].source];
-        edges[i].target = nodes[edges[i].target];
-
-        incidence_matrix[edges[i].source.index + "," + edges[i].target.index] = 1;
-    }
-}
-
-/**********************
- **********************
- *  *
- **********************
- **********************/
-
-function activateZoom(container)
-{
-    svg
-    .call(
-    d3.zoom()
-        .scaleExtent([.1, 4])
-        .on("zoom", function() { container.attr("transform", d3.event.transform); })
-);
-}
+document.getElementById('nodes').addEventListener('change', readNodes, false);
+document.getElementById('edges').addEventListener('change', readEdges, false);
 
 function drawGraph()
 {
     filterNodes();
     filterEdges();
-    nodesInitialization();
-    edgesInitialization();
-    initializeGlobalVariablesFromDOM();
 
-    // clean if is not the first time
-    if (svg != null)
-    {
-        svg.remove();
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    var label = {
+        'nodes': [],
+        'links': []
+    };
+
+    nodes.forEach(function(d, i) {
+        label.nodes.push({node: d});
+        label.nodes.push({node: d});
+        label.links.push({
+            source: i * 2,
+            target: i * 2 + 1
+        });
+    });
+
+    var labelLayout = d3.forceSimulation(label.nodes)
+        .force("charge", d3.forceManyBody().strength(-50))
+        .force("link", d3.forceLink(label.links).distance(0).strength(2));
+
+    var graphLayout = d3.forceSimulation(nodes)
+        .force("charge", d3.forceManyBody().strength(-3000))
+        .force("center", d3.forceCenter(width / 2, height / 2))
+        .force("x", d3.forceX(width / 2).strength(1))
+        .force("y", d3.forceY(height / 2).strength(1))
+        .force("link", d3.forceLink(edges).id(function(d) {return d.id; }).distance(50).strength(1))
+        .on("tick", ticked);
+
+    var adjlist = [];
+
+    edges.forEach(function(d) {
+        adjlist[d.source.index + "-" + d.target.index] = true;
+        adjlist[d.target.index + "-" + d.source.index] = true;
+    });
+
+    function neigh(a, b) {
+        return a == b || adjlist[a + "-" + b];
     }
 
-    let color = d3.scaleOrdinal(d3.schemeCategory10);
+    var svg = d3.select("#viz").attr("width", width).attr("height", height).attr("style", "border-style: solid;");
+    var container = svg.append("g");
 
-    // create svg
-    svg = d3
-        .select("body")
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("id", "graph")
-        .attr("style", "border-style: solid;");
+    svg.call(
+        d3.zoom()
+            .scaleExtent([.1, 4])
+            .on("zoom", function() { container.attr("transform", d3.event.transform); })
+    );
 
-    let container = svg.append("g");
-
-    activateZoom(container);
-
-    if (DEBUG)
-    {
-        console.log("Nodes");
-        console.log(nodes);
-        console.log("Edges");
-        console.log(edges);
-    }
-
-    // draw edges
-    edges_displayed = container
-        .append("g")
-        .attr("class", "links")
+    var link = container.append("g").attr("class", "links")
         .selectAll("line")
         .data(edges)
         .enter()
         .append("line")
         .attr("stroke", "#aaa")
-        .attr("stroke-width", "1px")
-        .attr("x1", d => d.x1)
-        .attr("y1", d => d.y1)
-        .attr("x2", d => d.x2)
-        .attr("y2", d => d.y2);
+        .attr("stroke-width", "1px");
 
-    // draw nodes
-    nodes_displayed = container
-        .append("g")
-        .attr("class", "nodes")
+    var node = container.append("g").attr("class", "nodes")
         .selectAll("g")
         .data(nodes)
         .enter()
         .append("circle")
         .attr("r", 5)
         .attr("fill", function(d) { return color(d.group); })
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y);
 
-    let iterations = document.getElementById("iterations").value;
+    node.on("mouseover", focus).on("mouseout", unfocus);
 
-    for (let i = 0; i < iterations; i++)
-    {
-            electrostaticRepulsion();
-            springForces();
+    node.call(
+        d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended)
+    );
+
+    var labelNode = container.append("g").attr("class", "labelNodes")
+        .selectAll("text")
+        .data(label.nodes)
+        .enter()
+        .append("text")
+        .text(function(d, i) { return i % 2 == 0 ? "" : d.node.name; })
+        .style("fill", "#555")
+        .style("font-family", "Arial")
+        .style("font-size", 12)
+        .style("pointer-events", "none"); // to prevent mouseover/drag capture
+
+    node.on("mouseover", focus).on("mouseout", unfocus);
+
+    function ticked() {
+
+        node.call(updateNode);
+        link.call(updateLink);
+
+        labelLayout.alphaTarget(0.3).restart();
+        labelNode.each(function(d, i) {
+            if(i % 2 == 0) {
+                d.x = d.node.x;
+                d.y = d.node.y;
+            } else {
+                var b = this.getBBox();
+
+                var diffX = d.x - d.node.x;
+                var diffY = d.y - d.node.y;
+
+                var dist = Math.sqrt(diffX * diffX + diffY * diffY);
+
+                var shiftX = b.width * (diffX - dist) / (dist * 2);
+                shiftX = Math.max(-b.width, Math.min(0, shiftX));
+                var shiftY = 16;
+                this.setAttribute("transform", "translate(" + shiftX + "," + shiftY + ")");
+            }
+        });
+        labelNode.call(updateNode);
+
     }
 
-}
+    function fixna(x) {
+        if (isFinite(x)) return x;
+        return 0;
+    }
 
-document.getElementById('nodes').addEventListener('change', readNodes, false);
-document.getElementById('edges').addEventListener('change', readEdges, false);
+    function focus(d) {
+        var index = d3.select(d3.event.target).datum().index;
+        node.style("opacity", function(o) {
+            return neigh(index, o.index) ? 1 : 0.1;
+        });
+        labelNode.attr("display", function(o) {
+            return neigh(index, o.node.index) ? "block": "none";
+        });
+        link.style("opacity", function(o) {
+            return o.source.index == index || o.target.index == index ? 1 : 0.1;
+        });
+    }
+
+    function unfocus() {
+        labelNode.attr("display", "block");
+        node.style("opacity", 1);
+        link.style("opacity", 1);
+    }
+
+    function updateLink(link) {
+        link.attr("x1", function(d) { return fixna(d.source.x); })
+            .attr("y1", function(d) { return fixna(d.source.y); })
+            .attr("x2", function(d) { return fixna(d.target.x); })
+            .attr("y2", function(d) { return fixna(d.target.y); });
+    }
+
+    function updateNode(node) {
+        node.attr("transform", function(d) {
+            return "translate(" + fixna(d.x) + "," + fixna(d.y) + ")";
+        });
+    }
+
+    function dragstarted(d) {
+        d3.event.sourceEvent.stopPropagation();
+        if (!d3.event.active) graphLayout.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+
+    function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    }
+
+    function dragended(d) {
+        if (!d3.event.active) graphLayout.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+    }
+}
