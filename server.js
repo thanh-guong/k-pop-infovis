4baf49178abb438625bddaa1745bfee3d2f88206
Marte Valerio Falcone <marte.vfalcone@gmail.ocm>
2020-07-14 12:24:01 +0200

    Working dockerized AS

--- server.js
+++ server.js
@@ -1,14 +1,53 @@
-const express = require('express');
+var fs = require('fs');
+var express = require('express');
+var app = express();
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+/***
+ * index route
+ */
+fs.readFile('index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    app.get('/', function (req, res) {
+        res.writeHeader(200, {"Content-Type": "text/html"});
+        res.write(html);
+        res.end();
+    });
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+/***
+ * javascript route
+ */
+fs.readFile('js/k-pop.js', function (err, js) {
+    if (err) {
+        throw err;
+    }
+    app.get('/js/k-pop.js', function (req, res) {
+        res.writeHeader(200, {"Content-Type": "text/javascript"});
+        res.write(js);
+        res.end();
+    });
+});
+
+/***
+ * css route
+ */
+fs.readFile('css/index.css', function (err, js) {
+    if (err) {
+        throw err;
+    }
+    app.get('/css/index.css', function (req, res) {
+        res.writeHeader(200, {"Content-Type": "text/css"});
+        res.write(js);
+        res.end();
+    });
+});
+
+app.listen(PORT, function () {
+    console.log('K-pop app running on port [' + PORT + '] \nPress CTRL+C to terminate');
+});
