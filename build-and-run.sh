f108a9f2ba866b30cc28cb763a86ee27ff47329d
Marte Valerio Falcone <marte.vfalcone@gmail.ocm>
2020-07-14 12:34:00 +0200

    Docker build and run script

---
+++ build-and-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PORT = 49160
+CONTAINER_TAG = k-pop-infovis
+
+if [ -z $1 ];
+	PORT = $1
+else
+	echo "Using default port $PORT. You can set your own port by typing sh $0 <port-number>"
+fi
+
+docker build -t $CONTAINER_TAG .
+docker run -p $PORT:8080 -d $CONTAINER_TAG
