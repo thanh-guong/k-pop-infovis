11bf74d4f217aa1531b21982e0f37937ee607573
Marte Valerio Falcone <marte.vfalcone@gmail.ocm>
2020-07-14 13:06:29 +0200

    Added package.json

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,7 +2,7 @@
 PORT = 49160
 CONTAINER_TAG = k-pop-infovis
 
-if [ -z $1 ];
+if [ -z $1 ]; then
 	PORT = $1
 else
 	echo "Using default port $PORT. You can set your own port by typing sh $0 <port-number>"
